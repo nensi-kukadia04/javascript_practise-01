8c639d640610f2e189456e0324bfd0079f513643
nensi-kukadia04 <kukadiyanensi838@gmail.com>
2024-11-19 18:49:11 +0530

    output_lecture-02

---
+++ custom.js
@@ -0,0 +1,18 @@
+// 1.console method
+
+console.log("srushtii ben");
+console.error("ashish bhai");
+console.warn("mitul bhai");
+
+// 2.document.write()
+
+document.write("hello world");
+
+// 3.alert()
+
+alert("police aa gayiii...");
+
+// 4.DOM method 
+
+document.getElementById('tag').innerHTML="helloew world";
+document.getElementById('tag').textContent="meowww"
